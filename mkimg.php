3c6dc1c9e56f57e0809e1e7579c289caeffa0b1c
Jeroen Domburg <jeroen@spritesmods.com>
2015-02-11 14:31:03 +0100

    Added icons, prettified script generating bitmaps for icons

--- mkimg.php
+++ mkimg.php
@@ -3,19 +3,25 @@
 //Small script to convert an 800x600 png into a .bm file the
 //eink display can render from its internal espfs.
 
-$im=imagecreatefrompng("apconnect.png");
-
-for ($y=0; $y<600; $y++) {
-	for ($x=0; $x<800; $x+=8) {
-		$b=0;
-		for ($z=0; $z<8; $z++) {
-			$b<<=1;
-			$c=imagecolorat($im, $x+$z, $y);
-			if ((($c)&0xff)<0x80) $b|=1;
+function convertImage($file, $out)
+	$im=imagecreatefrompng(file);
+	$of=fopen($out, "w");
+	for ($y=0; $y<600; $y++) {
+		for ($x=0; $x<800; $x+=8) {
+			$b=0;
+			for ($z=0; $z<8; $z++) {
+				$b<<=1;
+				$c=imagecolorat($im, $x+$z, $y);
+				if ((($c)&0xff)<0x80) $b|=1;
+			}
+			fprintf($of, "%c", $b);
 		}
-		printf("%c", $b);
 	}
+	imagedestroy($im)
+	fclose($of);
 }
+
+convertImage("icons/apconnect.png", "html/apconnect.bm");
 
 
 ?>
